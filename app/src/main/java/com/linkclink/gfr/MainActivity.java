5a184d35d375a16eeb8ce436a78317339b499240
LinkClink <shekaqwe@gmail.com>
2020-05-04 00:58:38 +0200

    Set splash screen

--- app/src/main/java/com/linkclink/gfr/MainActivity.java
+++ app/src/main/java/com/linkclink/gfr/MainActivity.java
@@ -2,13 +2,13 @@ package com.linkclink.gfr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }

